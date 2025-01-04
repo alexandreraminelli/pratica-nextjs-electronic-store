74f3f8315c48bd008444acf0e3ba474fc7c2de24
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-04 18:26:46 -0300

    chore: definição parcial de cores

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
+        // Cores do tema
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        // Cores secundárias
+        charcoal: "#2E2E2E",
+        white: {
+          smoke: "#F5F5F5",
+        },
+        gray: {
+          dim: "#656565",
+          battleship: "#989898",
+        },
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
