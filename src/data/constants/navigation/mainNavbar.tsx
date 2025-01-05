9af97c5a99c6887f4625e93d9d815296c9f2d59d
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-05 18:46:26 -0300

    feat: modelo e constante dos links do navbar

---
+++ src/data/constants/navigation/mainNavbar.tsx
@@ -0,0 +1,13 @@
+import NavLink from "@/data/model/navigation/NavLink"
+
+/**
+ * Links do navbar principal presente no header.
+ *
+ * @author Alexandre Raminelli
+ */
+export const mainNavbar: NavLink[] = [
+  { text: "Home", href: "/" },
+  { text: "About", href: "/about" },
+  { text: "Contact Us", href: "/contact" },
+  { text: "Blog", href: "/blog" },
+]
---
+++ src/data/model/navigation/NavLink.tsx
@@ -0,0 +1,11 @@
+/**
+ * Interface dos links de navegação.
+ *
+ * @author Alexandre Raminelli
+ */
+export default interface NavLink {
+  /** Endereço do link. */
+  href: string
+  /** Texto exibido no link. */
+  text: string
+}
