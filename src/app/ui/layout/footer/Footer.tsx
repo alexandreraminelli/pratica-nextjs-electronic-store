6ce46b6aef3ef4c9947d5b81c272b9c50b1a2b25
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-07 19:01:43 -0300

    feat: estrutura do footer

--- src/app/ui/layout/footer/Footer.tsx
+++ src/app/ui/layout/footer/Footer.tsx
@@ -1,3 +1,5 @@
+import LogoLink from "@/app/ui/logo/LogoLink"
+
 /**
  * Rodapé do site.
  *
@@ -7,18 +9,42 @@ export default function Footer() {
   return (
     <footer
       className="
+      flex flex-col items-center
+      gap-8 md:gap-6
       bg-dark-27 lg:bg-black
       text-light-9
       py-12 pd-container
       "
     >
-      <p>Footer</p>
-
-      {/* Logo */}
-
-      {/* Footer Nav */}
+      <div
+        className="
+        flex flex-col items-center md:flex-row md:justify-between
+        gap-8
+        w-full
+        "
+      >
+        {/* Logo */}
+        <FooterLogo />
+        {/* Footer Nav */}
+        <nav>Footer Navigation</nav>
+      </div>
 
       {/* Social Networks */}
+      <div>Social Networks</div>
     </footer>
+  )
+}
+
+/**
+ * Logo do footer.
+ *
+ * @author Alexandre Raminelli
+ */
+function FooterLogo() {
+  return (
+    <header>
+      {/* Logo */}
+      <LogoLink className="max-w-24 text-white" />
+    </header>
   )
 }
