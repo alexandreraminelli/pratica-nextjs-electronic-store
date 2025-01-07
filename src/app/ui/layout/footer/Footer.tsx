816505f2a105c334c9dfee625c6d577598be611b
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-07 18:31:59 -0300

    feat: criação do componente Footer

---
+++ src/app/ui/layout/footer/Footer.tsx
@@ -0,0 +1,24 @@
+/**
+ * Rodapé do site.
+ *
+ * @author Alexandre Raminelli
+ */
+export default function Footer() {
+  return (
+    <footer
+      className="
+      bg-dark-27 lg:bg-black
+      text-light-9
+      py-12 pd-container
+      "
+    >
+      <p>Footer</p>
+
+      {/* Logo */}
+
+      {/* Footer Nav */}
+
+      {/* Social Networks */}
+    </footer>
+  )
+}
