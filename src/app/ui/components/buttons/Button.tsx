727b28b93102a24b45122463c82bdcdf7cce1e81
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-08 18:29:29 -0300

    feat: efeito hover nos botões

--- src/app/ui/components/buttons/Button.tsx
+++ src/app/ui/components/buttons/Button.tsx
@@ -36,16 +36,16 @@ export default function Button(
     <button
       className={clsx(
         // Estilos gerais:
-        "flex flex-row cursor-pointer hover:shadow-md transition-all",
+        "flex flex-row gap-2 items-center justify-center cursor-pointer hover:shadow-md transition-all",
         // Condicionais:
         {
           "text-base rounded-md py-4 px-14": size === "normal", // normal size
           "text-sm rounded-lg py-3 px-16": size === "small", // small size
 
-          "bg-black text-white": state === "fill", // fill
+          "bg-black text-white hover:bg-zinc-700": state === "fill", // fill
           border: state === "white-stroke" || state === "black-stroke", // stroke
-          "border-white text-white": state === "white-stroke", // white stroke
-          "border-black text-black": state === "black-stroke", // black stroke
+          "border-white text-white hover:bg-zinc-500": state === "white-stroke", // white stroke
+          "border-black text-black hover:bg-zinc-600 hover:text-white": state === "black-stroke", // black stroke
         }
       )}
     >
