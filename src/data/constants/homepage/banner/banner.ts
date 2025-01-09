c01c62a78e2973fb70d4ec9172e5197fa16494c1
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-09 17:34:29 -0300

    feat: model e const de dados do banner

---
+++ src/data/constants/homepage/banner/banner.ts
@@ -0,0 +1,17 @@
+import BannerModel from "@/data/model/banners/BannerModel"
+
+// Caminhos das imagens
+const bannerImgRoute = "/pages/home/banners/"
+
+/**
+ * Conteúdo do banner principal.
+ *
+ * @author Alexandre Raminelli
+ */
+export const mainBanner: BannerModel = {
+  product: "IPhone 14 Pro",
+  description: "Created to change everything for the better. For everyone",
+  slogan: "Pro.Beyond.",
+  image: `${bannerImgRoute}iphone.png`,
+  link: "/smartphones", // link de teste
+}
---
+++ src/data/model/banners/BannerModel.ts
@@ -0,0 +1,18 @@
+/**
+ * Modelo de conteúdo do banner.
+ *
+ * @author Alexandre Raminelli
+ */
+export default interface BannerModel {
+  /** Produto exibido no banner. */
+  product: string
+  /** Breve descrição. */
+  description: string
+  /** Slogan do produto. */
+  slogan?: string
+  /** Imagem do produto. */
+  image: string
+
+  /** Link pra página do produto. */
+  link: string
+}
