34f528a1191526f38174b2e9906d4d19ec5385da
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-10 19:59:14 -0300

    feat(cta): conteúdo do FooterBanner

--- src/app/ui/components/banners/FooterBanner.tsx
+++ src/app/ui/components/banners/FooterBanner.tsx
@@ -1,8 +1,29 @@
+import ButtonLink from "@/app/ui/components/buttons/ButtonLink"
+import { bigSummerSaleBanner } from "@/data/constants/homepage/banner/banner" // dados do banner
+
+// desestruturação
+const { title, subtitle, href } = bigSummerSaleBanner
+
 /**
  * Banner da tela inicial exibido acima do footer.
  *
  * @author Alexandre Raminelli
  */
 export default function FooterBanner() {
-  return <div>Footer Banner</div>
+  return (
+    <aside>
+      <div>
+        {/* Título */}
+        <h2>
+          {title.normal} <strong>{title.bolder}</strong>
+        </h2>
+
+        {/* Subtítulo */}
+        <p>{subtitle}</p>
+      </div>
+
+      {/* Botão */}
+      <ButtonLink href={href} text="Shop Now" state="white-stroke" />
+    </aside>
+  )
 }
