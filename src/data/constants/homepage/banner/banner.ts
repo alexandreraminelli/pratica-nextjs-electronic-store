86da4818f8cb5efff804e53d33e80779ebc57f25
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-10 16:24:04 -0300

    fix: imagens dos banners que não estavam visíveis

--- src/data/constants/homepage/banner/banner.ts
+++ src/data/constants/homepage/banner/banner.ts
@@ -1,7 +1,7 @@
 import BannerModel from "@/data/model/banners/BannerModel"
 
 // Caminhos das imagens
-const bannerImgRoute = "/pages/home/banners/"
+const bannerImgRoute = "/pages/home/banners"
 
 /**
  * Conteúdo do banner principal.
@@ -12,8 +12,8 @@ export const mainBanner: BannerModel = {
   product: "IPhone 14 Pro",
   description: "Created to change everything for the better. For everyone",
   slogan: "Pro.Beyond.",
-  image: `${bannerImgRoute}iphone.png`,
-  imageDesktop: `${bannerImgRoute}iphone-lg.png`,
+  image: `${bannerImgRoute}/iphone.png`,
+  imageDesktop: `${bannerImgRoute}/iphone-lg.png`,
   link: "/smartphones", // link de teste
 }
 
