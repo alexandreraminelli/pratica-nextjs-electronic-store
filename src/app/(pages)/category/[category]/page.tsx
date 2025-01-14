9f2f69e0a716616da36b00945c494e0e0c9dfdd5
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-14 11:52:05 -0300

    feat: criação da rota dinâmica de categorias

---
+++ src/app/(pages)/category/[category]/page.tsx
@@ -0,0 +1,34 @@
+/**
+ * Interface dos props de rota dinâmica.
+ */
+interface CategoryPageProps {
+  /**
+   * Parâmetros de rota dinâmica.
+   * Define o formato esperado para os props recebidos pelo componente da página.
+   *
+   * - `params`: parâmetros do sistema de rotas dinâmicas do Next.js
+   * - `Promise<{ category: string }>`: retorna objeto contento um parâmetro `category`.
+   */
+  params: Promise<{ category: string }>
+}
+/**
+ * Página dinâmica de categorias.
+ * Exibe os produtos que pertencem a categoria pesquisada.
+ *
+ * @param param0 Props da rota dinâmica desestruturados.
+ *
+ * @author Alexandre Raminelli
+ */
+export default async function CategoryPage(
+  { params }: CategoryPageProps // definir parâmetros props da página
+) {
+  /** Extração do parâmetro `category` do `params`. */
+  const category = (await params).category
+
+  // retorno da página
+  return (
+    <div className="pd-container py-20">
+      <h1 className="text-center text-5xl">Página da categoria {category}</h1>
+    </div>
+  )
+}
