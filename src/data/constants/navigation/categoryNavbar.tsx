b8bab6c6a50902b66e5b7195ca0055162d3e7b50
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-14 12:08:28 -0300

    feat(category): edição dos links das rotas de categorias

--- src/data/constants/navigation/categoryNavbar.tsx
+++ src/data/constants/navigation/categoryNavbar.tsx
@@ -1,16 +1,19 @@
 import { NavLinkWithIcon } from "@/data/model/navigation/NavLink"
 import { CameraIcon, Gamepad2Icon, HeadphonesIcon, LaptopIcon, SmartphoneIcon, WatchIcon } from "lucide-react" // ícones
 
+/** Rota de pesquisa de categoria. */
+const categoryRoute: string = "/category"
+
 /**
  * Links do navbar de categorias.
  *
  * @author Alexandre Raminelli
  */
 export const categoryNavbar: NavLinkWithIcon[] = [
-  { text: "Phones", href: "/smartphones", Icon: SmartphoneIcon },
-  { text: "Computers", href: "/computers", Icon: LaptopIcon },
-  { text: "Smart Watches", href: "/smartwatches", Icon: WatchIcon },
-  { text: "Cameras", href: "/cameras", Icon: CameraIcon },
-  { text: "Headphones", href: "/headphones", Icon: HeadphonesIcon },
-  { text: "Gaming", href: "/gaming", Icon: Gamepad2Icon },
+  { text: "Phones", href: `${categoryRoute}/smartphones`, Icon: SmartphoneIcon },
+  { text: "Computers", href: `${categoryRoute}/computers`, Icon: LaptopIcon },
+  { text: "Smart Watches", href: `${categoryRoute}/smartwatches`, Icon: WatchIcon },
+  { text: "Cameras", href: `${categoryRoute}/cameras`, Icon: CameraIcon },
+  { text: "Headphones", href: `${categoryRoute}/headphones`, Icon: HeadphonesIcon },
+  { text: "Gaming", href: `${categoryRoute}/gaming`, Icon: Gamepad2Icon },
 ]
