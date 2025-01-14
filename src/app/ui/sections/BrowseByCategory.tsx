45d6657c9b7f804f51e7783dad7280c0c940af08
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-14 14:31:48 -0300

    feat: criação da seção BrowseByCategory

---
+++ src/app/ui/sections/BrowseByCategory.tsx
@@ -0,0 +1,24 @@
+/**
+ * Seção de buscar produtos por categoria.
+ * Possui um grupo de cards com um link que abre o catálogo de produtos na categoria.
+ *
+ * @author Alexandre Raminelli
+ */
+export default function BrowseByCategory() {
+  return (
+    <section
+      className="flex flex-col
+        pd-container py-16 gap-12
+        bg-color-26"
+    >
+      <header>
+        {/* Título */}
+        <h2 className="font-medium text-2xl">Browse By Category</h2>
+
+        {/* Setas de paginação */}
+      </header>
+
+      {/* Cards de categorias */}
+    </section>
+  )
+}
