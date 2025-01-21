d5fd6b78306b13a70d789a141dfbc589bc1fb335
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-01-21 15:57:59 -0300

    fix(database): correção do commit anterior

--- src/utils/database/createTables/seedProduct.ts
+++ src/utils/database/createTables/seedProduct.ts
@@ -13,7 +13,7 @@ export default async function seedProduct() {
   await client.sql`
     CREATE TABLE IF NOT EXISTS product (
         product_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY, -- identificador
-        brand UUID NOT NULL REFERENCES brand(brand_id) -- marca do produto
+        brand UUID NOT NULL REFERENCES brand(brand_id), -- marca do produto
         name VARCHAR(45) NOT NULL UNIQUE, -- nome do produto
         price_in_cents INT NOT NULL,  -- preço (em centavos)
         description TEXT, -- descrição do produto
