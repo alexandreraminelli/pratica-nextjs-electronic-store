46a4e091d7208a66ef218eb743c0938ea3062bef
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-02-03 19:33:19 -0300

    feat(db): criação de hook que obtêm os produtos do banco de dados

---
+++ src/data/hooks/database/useProducts.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+/**
+ * Hook que obtêm os produtos do banco de dados usando uma API Rest.
+ * @author Alexandre Raminelli
+ */
+export function useProducts() {
+  // definição de estados
+  const [products, setProducts] = useState<any[]>([])
+  const [loading, setLoading] = useState(true)
+
+  // buscar os produtos
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        // Fazer requisição GET para o API Rest de produtos
+        const response = await fetch("/api/products")
+        const data = await response.json()
+        setProducts(data)
+      } catch (e) {
+        console.log("Error fetching products:", e)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchProducts()
+  }, [])
+
+  // Retornar os dados
+  return { products, loading }
+}
