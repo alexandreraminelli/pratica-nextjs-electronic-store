8b54ea0a57340c0df468d9fa4908ea8390f245f2
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-02-05 14:24:08 -0300

    feat(products): definição das imagens dos smartphones no db

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
     /** Domínios externos para obter imagens. */
     remotePatterns: [
       { hostname: "cdsassets.apple.com" }, // Apple
+      { hostname: "www.apple.com" }, // Apple
+      { hostname: "samsungbrshop.vtexassets.com" }, // Samsung
+      { hostname: "lh3.googleusercontent.com" }, // Google
+      { hostname: "m.media-amazon.com" }, // Amazon
+      { hostname: "www.oneplus.com" }, // One Plus
     ],
   },
 }
